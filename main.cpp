8439d317d4e4bd7c1d9d738e26e7d74b262bc7ae
jeaye <contact@jeaye.com>
2014-07-22 18:03:59 +0800

    Added expect and expect_equal

--- main.cpp
+++ main.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include <vector>
 #include <string>
+#include <sstream>
 #include <functional>
 #include <type_traits>
 #include <memory>
@@ -20,6 +21,9 @@ namespace jest
     using failure = std::string;
     using optional_failure = std::experimental::optional<std::experimental::optional<failure>>;
 
+    struct tally_results
+    { size_t const total, failed; };
+
     void log_failure(size_t const n, std::string const &msg)
     { std::cout << "  test " << n << " failure: " << msg << std::endl; }
     void log_success(size_t const n)
@@ -28,7 +32,7 @@ namespace jest
     struct group_concept
     {
       virtual ~group_concept() = default;
-      virtual void run() const = 0;
+      virtual tally_results run() const = 0;
     };
 
     class registrar
@@ -72,22 +76,64 @@ namespace jest
     }
 
     template <typename Group, size_t... Ns>
-    void run_impl(std::string const &name, std::integer_sequence<size_t, Ns...>)
+    tally_results run_impl(std::string const &name, std::integer_sequence<size_t, Ns...>)
     {
       std::cout << "running group '" + name << "'" << std::endl;
       detail::optional_failure const results[]{ test_impl<Group, Ns>()... };
+      size_t failed{};
       for(size_t i{}; i < sizeof...(Ns); ++i)
       {
         if(results[i])
         {
           if(results[i].value())
-          { log_failure(i, results[i].value().value()); }
+          {
+            log_failure(i, results[i].value().value());
+            ++failed;
+          }
           else
           { log_success(i); }
         }
       }
       std::cout << "finished group '" + name << "'\n" << std::endl;
+      return { sizeof...(Ns), failed };
     }
+
+    template <typename... Args>
+    void fail(std::string const &msg, Args const &... args)
+    {
+      std::stringstream ss;
+      ss << std::boolalpha;
+      int const _[]{ (ss << args << ", ", 0)... }; /* TODO: quotes around strings */
+      (void)_;
+      throw std::runtime_error{ "failed '" + msg + "' (" + ss.str().substr(0, ss.str().size() - 2) + ")" };
+    }
+
+    template <typename T1, typename T2>
+    void compare(T1 const &t1, T2 const &t2)
+    {
+      if(t1 != t2)
+      { fail("not equal", t1, t2); }
+    }
+    template <typename T, typename... Args>
+    void expect_equal_impl(size_t const n,
+                           T const &t, Args const &... args)
+    {
+      int const _[]
+      { (compare(t, args), 0)... };
+      (void)_;
+      if(n != sizeof...(Args))
+      { expect_equal_impl(n + 1, args..., t); }
+    }
+  }
+
+  template <typename... Args>
+  void expect_equal(Args const &... args)
+  { detail::expect_equal_impl(0, args...); }
+  template <typename... Args>
+  void expect(bool const b)
+  {
+    if(!b)
+    { detail::fail("unexpected", b); }
   }
 
   template <typename Data, size_t N = 50>
@@ -99,8 +145,8 @@ namespace jest
         : name_{ std::move(name) }
       { detail::registrar::get().add(*this); }
 
-      void run() const override
-      { detail::run_impl<group>(name_, std::make_integer_sequence<size_t, N>()); }
+      detail::tally_results run() const override
+      { return detail::run_impl<group>(name_, std::make_index_sequence<N>()); }
 
     private:
       std::string name_;
@@ -113,34 +159,41 @@ namespace jest
     void operator ()() const
     {
       auto &registrar(detail::registrar::get());
+      size_t failed{};
       for(auto const &group : registrar)
-      { group.get().run(); }
+      {
+        auto const tally(group.get().run());
+        failed += tally.failed;
+      }
+      if(failed)
+      { std::cout << failed << " test(s) failed" << std::endl; }
+      else
+      { std::cout << "all tests passed" << std::endl; }
     }
   };
 }
 
 struct ex_1 { };
 using ex_1_group = jest::group<ex_1>;
-ex_1_group ex_1_g{ "ctor" };
-struct ex_2 { };
-using ex_2_group = jest::group<ex_2>;
-ex_2_group ex_2_g{ "shared" };
+ex_1_group ex_1_g{ "example" };
 
 namespace jest
 {
   template <>
   void test<ex_1_group, 0>()
-  { }
+  {
+    int const i{};
+    float const f{ 3.14 };
+    expect(i == f);
+  }
   template <>
   void test<ex_1_group, 1>()
-  { throw std::runtime_error{ "not good" }; }
-
-  template <>
-  void test<ex_2_group, 0>()
-  { }
-  template <>
-  void test<ex_2_group, 1>()
-  { }
+  {
+    int const i{};
+    float const f{ 3.14 };
+    float const f2{ f * 2.0f };
+    expect_equal(i, f, f2);
+  }
 }
 
 int main()
